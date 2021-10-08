9e1d6876ff03c6ba38b600171e56add262ab9d58
Ксения Чемлекчиева <your_next_life@mail.ru>
2021-10-08 03:44:19 +0300

    Добавьте файлы проекта.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,7 +18,7 @@ namespace Exercise3
             double z = Convert.ToDouble(zstr);
 
             double f;
-            f = 1 / 13 + Math.Log(Math.Abs((y + 2) / 3)) + z + 3 + Math.Min(x, 1);
+            f = 1.0 / 13.0 + Math.Log(Math.Abs((y + 2.0) / 3.0)) + z + 3.0 + Math.Min(x, 1.0);
             Console.WriteLine(f);
         }
     }
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static double x;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            x = Double.Parse(Console.ReadLine());
+            double y = F(x);
+            Console.WriteLine("f({1})={0}", y, x);
+            Console.Read();
+
+        }
+        static double F(double X)
+        {
+            double res = (1.0 / 13.0) + Math.Log(Math.Abs((X + 2.0) / 3.0)) + X + 3.0 + Math.Min(X, 1.0) + 2.0 * Math.Cos(X / 3.0) + Math.Pow(X, X);
+            return res;
+
+        }
+    }
+}
+//16. f(x) = 1/13 + ln(|(x+2)/3|) + x+3 + min(x, 1) + 2cos(x/3) + x^x
